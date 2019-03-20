10c6b6de54d9feda97bd03761ddf0b41496100a0
Brian Wyant <bwyant@helloinspire.com>
2019-03-20 14:33:12 -0400

    feat: add simple rdoc-style documentation & readme examples

---
+++ README.md
@@ -0,0 +1,38 @@
+Simple metrics helper built to be agnostic of particular backends.
+
+Examples
+========
+
+##### Record simple event:
+
+```
+MetricsService.event('MessageHandledSuccess')
+```
+
+##### Record timings of operations:
+
+This example will push three metrics - one for the overall processing time, and one per step
+
+```
+def complex_method
+  start = Time.current
+  MetricsService.time_operation('complex_method_overall') do
+    puts 'Step 1...'
+    sleep 10
+    MetricsService.duration_start_end('step_1', start, Time.current)
+    after_step_1 = Time.current
+    puts 'Step 2...'
+    sleep 30
+    MetricsService.duration_start_end('step_2', after_step_1, Time.current)
+  end
+end
+```
+
+##### Record an exception:
+
+```
+rescue StandardError => e
+  MetricsService.exception('suppressable_path', e)
+end
+```
+
--- lib/metrics_service.rb
+++ lib/metrics_service.rb
@@ -1,9 +1,21 @@
+# frozen_string_literal: true
+
+##
+# An object with methods for tracking basic, consistent metrics
 class MetricsService
+  ##
+  # Track a basic event; intended for COUNT queries. Outputs `TrackedEvent`
+  #
+  # @param [String] name A name for filtering within WHERE queries
   def self.event(name, **additional_properties)
-    properties = {name: name}.merge(additional_properties)
-    ::NewRelic::Agent.record_custom_event("TrackedEvent", properties)
+    properties = { name: name }.merge(additional_properties)
+    ::NewRelic::Agent.record_custom_event('TrackedEvent', properties)
   end
 
+  ##
+  # Given a name and a block, tracks the milliseconds involved in processing the operation. Outputs `OpDurationMs`
+  #
+  # @param [String] name A name for filtering within WHERE queries
   def self.time_operation(name, **additional_properties)
     raise 'A block is required for time_operation calls' unless block_given?
 
@@ -14,26 +26,46 @@ class MetricsService
     Rails.logger.error("Failed reporting timing to NewRelic: #{e}")
   end
 
+  ##
+  # Given a name and a start/finish time, tracks the milliseconds involved in processing the operation. Outputs `OpDurationMs`
+  #
+  # @param [String] name A name for filtering within WHERE queries
+  # @param [Time] start The start time of the operation
+  # @param [Time] end The end time of the operation
   def self.duration_start_end(name, start, finish, **additional_properties)
     duration(name, (finish - start).seconds, **additional_properties)
   rescue StandardError => e
     Rails.logger.error("Failed reporting duration to NewRelic: #{e}")
   end
 
+  ##
+  # Given a name and a duration, tracks the milliseconds involved in processing the operation. Outputs `OpDurationMs`
+  #
+  # @param [String] name A name for filtering within WHERE queries
+  # @param [Time] start The start time of the operation
+  # @param [Time] end The end time of the operation
   def self.duration(name, duration, **additional_properties)
-    properties = {name: name, duration: duration.in_milliseconds}.merge(additional_properties)
-    ::NewRelic::Agent.record_custom_event("OpDurationMs", properties)
+    properties = { name: name, duration: duration.in_milliseconds }.merge(additional_properties)
+    ::NewRelic::Agent.record_custom_event('OpDurationMs', properties)
   rescue StandardError => e
     Rails.logger.error("Failed reporting duration to NewRelic: #{e}")
   end
 
+  ##
+  # Tracks an exception for error reporting when errors are suppressable for the purpose of HTTP status codes. Outputs `Error`
+  #
+  # @param [String] name A name for filtering within WHERE queries
   def self.exception(name, exception)
     error(name, type: exception.class.name, message: exception&.message)
   end
 
+  ##
+  # Tracks an error for error reporting with arbitrary additional named argument properties. Outputs `Error`
+  #
+  # @param [String] name A name for filtering within WHERE queries
   def self.error(name, **additional_properties)
-    properties = {error_name: name}.merge(additional_properties)
-    ::NewRelic::Agent.record_custom_event("Error", properties)
+    properties = { error_name: name }.merge(additional_properties)
+    ::NewRelic::Agent.record_custom_event('Error', properties)
   rescue StandardError => e
     Rails.logger.error("Failed reporting error to NewRelic: #{e}")
   end
